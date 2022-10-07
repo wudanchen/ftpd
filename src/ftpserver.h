cc6c71f6ddec5c19cc219b8f1d6b9c5a7e663393
wdc <724214532@qq.com>
2022-10-07 04:16:32 +0000

    [feature] 修改cmake 文件

--- src/ftpserver.h
+++ src/ftpserver.h
@@ -2,7 +2,7 @@
  * @Author: wdc 724214532@qq.com
  * @Date: 2022-10-01 17:35:28
  * @LastEditors: wdc 724214532@qq.com
- * @LastEditTime: 2022-10-07 01:21:31
+ * @LastEditTime: 2022-10-07 01:49:42
  * @FilePath: /ftpd/ftpserver.h
  * @Description: 
  * 
@@ -14,7 +14,7 @@
 
 class Ftp_Server {
 public:
-    Ftp_Server(int port);
+    explicit Ftp_Server(int port);
     ~Ftp_Server();
     int run_loop();
 
