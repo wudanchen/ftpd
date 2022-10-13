e056191997f3e6b8be0f4097b129bc18abb6c67e
wudancheng <wudancheng@scutech.com>
2022-10-13 18:29:34 +0800

    refs #65543 Build a gtest case framework

---
+++ test/gtest_commandparser.cpp
@@ -0,0 +1,9 @@
+
+#include <gtest/gtest.h>
+#include "commandparser.h"
+
+TEST(CommandParser, handle) {
+    Command_Parser parser;
+    EXPECT_EQ(1, 1);
+}
+
---
+++ test/gtest_userinfo.cpp
@@ -0,0 +1,9 @@
+
+#include <gtest/gtest.h>
+#include "userinfo.h"
+
+TEST(UserInfo, check) {
+    User_Info info;
+    EXPECT_EQ(1, 1);
+}
+
