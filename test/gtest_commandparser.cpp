18a833df7c8bb956663699fd42952e43fddfb211
wudancheng <wudancheng@scutech.com>
2022-10-13 19:30:34 +0800

    [feature] 添加修改

--- test/gtest_commandparser.cpp
+++ test/gtest_commandparser.cpp
@@ -1,9 +1,16 @@
 
 #include <gtest/gtest.h>
 #include "commandparser.h"
+#include "constants.h"
 
-TEST(CommandParser, handle) {
+#include <iostream>
+
+TEST(CommandParser, UserHandle) {
     Command_Parser parser;
-    EXPECT_EQ(1, 1);
+    std::string buf = "uSeR \r\n";
+    parser.parsing_data(buf.c_str(), buf.size());
+    EXPECT_STREQ(msg_login_fail, parser.response_data());
+    buf.assign("USER scutech\r\n");
+    parser.parsing_data(buf.c_str(), buf.size());
+    EXPECT_STREQ(msg_user_require_pass, parser.response_data());
 }
-
--- test/gtest_userinfo.cpp
+++ test/gtest_userinfo.cpp
@@ -2,8 +2,12 @@
 #include <gtest/gtest.h>
 #include "userinfo.h"
 
-TEST(UserInfo, check) {
+//测试密码
+TEST(UserInfo, password) {
     User_Info info;
-    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(info.check_password("scutech", "dingjia"));
+    EXPECT_TRUE(info.check_password("xiaobai", "12456"));
+    EXPECT_FALSE(info.check_password("xiaoming", "haha"));
+    EXPECT_FALSE(info.check_password("xiaohei", "hehe"));
 }
 
